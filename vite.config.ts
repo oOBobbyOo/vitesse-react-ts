85b2fdf0fa1a3f1817c95acfca807fb01befa783
oOBobbyOo <944831575@qq.com>
2026-07-09 11:41:03 +0800

    feat: lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath } from 'node:url'
 
 import babel from '@rolldown/plugin-babel'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import browserslist from 'browserslist'
+import { browserslistToTargets } from 'lightningcss'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -16,5 +18,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist(['> 1%', 'last 2 versions'])),
+    },
+  },
+  build: {
+    cssMinify: 'lightningcss',
   },
 })
