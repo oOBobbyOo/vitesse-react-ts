591f4811997ae006fab4fa39cc9d9fd68574e336
oOBobbyOo <944831575@qq.com>
2026-07-09 11:16:35 +0800

    fix: rename router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router/dom'
 
-import router from '@/routes'
+import router from '@/router'
 
 import '@/styles/index.css'
 
